8aaac3f33e41b718d5cc96d6f12bf3dece9ca4a5
codehoper <vsraje01@gmail.com>
2013-01-24 02:43:47 -0500

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -1,6 +1,11 @@
-
 sudo apt-get install ctags
-mkdir ~/.vim/plugin
-cd ~/.vim/plugin/ && wget -O taglist.zip http://www.vim.org/scripts/download_script.php?src_id=7701 && unzip taglist.zip && rm -r taglist.zip
-sudo apt-get install curl && mkdir -p ~/.vim/autoload ~/.vim/bundle; curl -so ~/.vim/autoload/pathogen.vim     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
-.vim/bundle && git clone https://github.com/scrooloose/nerdtree.git
+sudo apt-get install curl
+#pollute with bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+#add just plugins
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
