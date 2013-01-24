d72ff9034e53b1e59fe54acd434976620838e5b8
codehoper <vsraje01@gmail.com>
2013-01-24 03:03:46 -0500

    Update vim_install.sh
    tlistplugin added

--- vim_install.sh
+++ vim_install.sh
@@ -11,3 +11,6 @@ https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 
+#install Tlist plugin
+cd ~/.vim/ && wget -O taglist.zip http://www.vim.org/scripts/download_script.php?src_id=7701 && unzip taglist.zip && rm -r taglist.zip
+
